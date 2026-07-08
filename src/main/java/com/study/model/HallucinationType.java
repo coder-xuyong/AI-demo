793d66a705c2b7e280d16698c4ad73b421e4234f
XuYong <1299461580@qq.com>
2026-07-08 20:50:47 +0800

    提交代码

---
+++ src/main/java/com/study/model/HallucinationType.java
@@ -0,0 +1,11 @@
+package com.study.model;
+
+
+public enum HallucinationType {
+    POLICY_FABRICATION,      // 政策与规则编造
+    PRODUCT_PARAM_FABRICATION,// 产品参数编造
+    CAPABILITY_OVERSTEP,     // 能力越界
+    SAFETY_MISLEADING,       // 安全与合规误导
+    INFORMATION_MISSING,     // 信息遗漏与错误
+    NONE                     // 无幻觉
+}
